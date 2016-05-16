9236c8b9cecfa2f97236ecf36a1ff167cf673400
heidiflammable <heidiflammable@gmail.com>
2016-05-15 18:44:25 -0700

    testing global add

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// implement your express server here
+'use strict';
+var express = require('express');
+var bodyparser = require('body-parser');
+var serveStatic = express.static('public');
+var app = express();
+var truckRouter = require('./routes/truckRoutes');
+
+app.use( serveStatic );
+app.use( bodyparser.urlencoded({ extended: false }) );
+app.use( bodyparser.json() );
+
+//this will get us to routes/truckRoutes if we
+// use /trucks in url which gets us acces to foodTrucks
+app.use( '/trucks', truckRouter );
+
+app.listen(3000, function () {
+  //console.log('server started on port 3000');
+});
+
+//  in this file create an express application - use the middle-ware built into express
+//  to serve up static files from the public directory (index.html and client.js - you
+//  can also serve up css files from the public directory this way if you want)
+//  you need to support a '/trucks' endpoint, and a dynamic route for '/trucks/:name'
--- routes/truckRoutes.js
+++ routes/truckRoutes.js
@@ -1,1 +1,52 @@
 // put your Express router code in here
+'use strict';
+//express
+var express = require('express');
+
+//the router
+var router = express.Router();
+
+//the list of trucks
+var foodTrucks = require('../trucks');
+
+
+router.route('/')
+  .get(function(request, response) {
+    response.send(foodTrucks.getTrucks());
+  })
+
+
+.post(function(request, response) {
+
+  //adding new truck
+  response.send(foodTrucks.addTruck(request.body));
+});
+
+//dynamic param - url driven getTruck via name
+router.route('/:name')
+  .get(function(request, response) {
+
+    //get truck via name
+    var truck = foodTrucks.getTruck(request.params.name);
+    if (truck) {
+
+      response.send(truck);
+
+    } else {
+
+      //truck not found server error
+      response.status(404).json('Sorry, ' + request.params.name + ' was \
+        not found.');
+    }
+  })
+
+
+//delete truck
+.delete(function(request, response) {
+
+  //remove existing truck
+  response.send(foodTrucks.removeTruck(request.params.name));
+
+});
+
+module.exports = router;
